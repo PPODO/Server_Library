b78d48a95fb7e28659f5d3e562dd2be61eb454a1
PPODO <sherlockwhooo@gmail.com>
2019-10-14 20:56:32 +0900

    OVERLAPPED_EX 구조체 멤버 변수 추가

--- Network/Socket/Socket.h
+++ Network/Socket/Socket.h
@@ -12,7 +12,7 @@ namespace NETWORK {
 	}
 
 	namespace SOCKET { 
-		const short INVALID_SOCKET_VALUE = static_cast<::SOCKET>(-1);
+		const short INVALID_SOCKET_VALUE = static_cast<::SOCKET>(0);
 		namespace BASESOCKET {
 			class CBaseSocket;
 		}
@@ -31,15 +31,21 @@ namespace NETWORK {
 					};
 
 					// 서버에서만 사용 가능합니다.
-					typedef struct OVERLAPPED_EX {
+					struct OVERLAPPED_EX {
 						WSAOVERLAPPED m_Overlapped;
+						// WSABUF에 읽은 데이터가 저장됨.
 						WSABUF m_WSABuffer;
+						// 처리하고 남은 바이트 수. 이전에 읽은 데이터가 남아 있을 경우, 데이터가 덮어씌워지는데 이를 방지하기 위한 변수임.
+						int16_t m_RemainReceivedBytes;
+						// 데이터가 덮어씌워지지 않도록 처리를 하였을데, 이전에 읽은 데이터가 남아있는 위치부터 현재 읽은 데이터까지 처리를 해야하는데, 이전에 읽은 데이터의 위치를 모르기 때문에, 그 위치를 저장하는 변수
+						char* m_SocketMessage;
+
 						EIOTYPE m_IOType;
 						NETWORK::SESSION::SERVERSESSION::CServerSession* m_Owner;
 
 					public:
-						OVERLAPPED_EX() : m_IOType(EIOTYPE::EIT_NONE), m_Owner(nullptr) { ZeroMemory(&m_Overlapped, sizeof(WSAOVERLAPPED)); };
-						OVERLAPPED_EX(const EIOTYPE& Type, NETWORK::SESSION::SERVERSESSION::CServerSession* Owner) : m_IOType(Type), m_Owner(Owner) { ZeroMemory(&m_Overlapped, sizeof(WSAOVERLAPPED)); };
+						OVERLAPPED_EX() : m_IOType(EIOTYPE::EIT_NONE), m_Owner(nullptr), m_RemainReceivedBytes(0), m_SocketMessage(nullptr) { ZeroMemory(&m_Overlapped, sizeof(WSAOVERLAPPED)); ZeroMemory(&m_WSABuffer, sizeof(WSABUF)); };
+						OVERLAPPED_EX(const EIOTYPE& Type, NETWORK::SESSION::SERVERSESSION::CServerSession* Owner) : m_IOType(Type), m_Owner(Owner), m_RemainReceivedBytes(0), m_SocketMessage(nullptr) { ZeroMemory(&m_Overlapped, sizeof(WSAOVERLAPPED)); ZeroMemory(&m_WSABuffer, sizeof(WSABUF)); };
 
 					};
 				}
@@ -85,6 +91,9 @@ namespace NETWORK {
 				void Destroy();
 
 			protected:
+				void CopyReceiveBuffer(char* const Buffer, const uint16_t& RecvSize);
+
+			protected:
 				inline char* const GetReceiveBufferPtr() { return m_ReceiveMessageBuffer; }
 				inline ::SOCKET GetSocketHandle() const { return m_Socket; }
 
@@ -94,7 +103,9 @@ namespace NETWORK {
 
 			public:
 				bool Bind(const FUNCTIONS::SOCKADDR::CSocketAddress& BindAddress);
-				void CopyReceiveBuffer(char* const Buffer, const uint16_t& RecvSize);
+
+			public:
+				inline ::SOCKET GetSocket() const { return m_Socket; }
 
 			};
 		}
@@ -107,7 +118,7 @@ namespace NETWORK {
 			}
 
 			inline NETWORK::ERRORCODE::ENETRESULT SetSockOption(const ::SOCKET& Socket, const int32_t& Level, const int32_t& OptionName, void* const OptionVariable, const size_t& OptionLen) {
-				if (setsockopt(Socket, Level, OptionName, reinterpret_cast<char*>(OptionVariable), OptionLen) == SOCKET_ERROR) {
+				if (setsockopt(Socket, Level, OptionName, reinterpret_cast<char*>(OptionVariable), reinterpret_cast<const int&>(OptionLen)) == SOCKET_ERROR) {
 					return NETWORK::ERRORCODE::ENETRESULT::ENETSOCKTOPFAIL;
 				}
 				return NETWORK::ERRORCODE::ENETRESULT::ENETSUCCESS;
