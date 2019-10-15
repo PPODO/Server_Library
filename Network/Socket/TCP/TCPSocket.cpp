df63994734b512f4e509b4bfbf688e60106bcea5
PPODO <sherlockwhooo@gmail.com>
2019-10-16 01:03:03 +0900

    소켓 리턴 함수명 변경

--- Network/Socket/TCP/TCPSocket.cpp
+++ Network/Socket/TCP/TCPSocket.cpp
@@ -13,7 +13,7 @@ CTCPIPSocket::CTCPIPSocket() : CBaseSocket(NETWORK::UTIL::BASESOCKET::EPROTOCOLT
 	OptionVar.l_linger = 0;
 
 	try {
-		if (NETWORK::UTIL::BASESOCKET::SetSockOption(GetSocketHandle(), SOL_SOCKET, SO_LINGER, &OptionVar, sizeof(LINGER)) == NETWORK::ERRORCODE::ENETRESULT::ENETSOCKTOPFAIL) {
+		if (NETWORK::UTIL::BASESOCKET::SetSockOption(GetSocket(), SOL_SOCKET, SO_LINGER, &OptionVar, sizeof(LINGER)) == NETWORK::ERRORCODE::ENETRESULT::ENETSOCKTOPFAIL) {
 			throw FUNCTIONS::EXCEPTION::bad_sockopt();
 		}
 	}
@@ -26,7 +26,7 @@ CTCPIPSocket::~CTCPIPSocket() {
 }
 
 bool CTCPIPSocket::Listen(const int32_t& BackLogCount) {
-	if (listen(GetSocketHandle(), BackLogCount) == SOCKET_ERROR) {
+	if (listen(GetSocket(), BackLogCount) == SOCKET_ERROR) {
 		CLog::WriteLog(L"Listen : Failed To Listen Process! - %d", WSAGetLastError());
 		return false;
 	}
@@ -34,7 +34,7 @@ bool CTCPIPSocket::Listen(const int32_t& BackLogCount) {
 }
 
 bool CTCPIPSocket::Connect(const FUNCTIONS::SOCKADDR::CSocketAddress& ConnectAddress) {
-	if (WSAConnect(GetSocketHandle(), &ConnectAddress, ConnectAddress.GetSize(), nullptr, nullptr, 0, 0) == SOCKET_ERROR) {
+	if (WSAConnect(GetSocket(), &ConnectAddress, ConnectAddress.GetSize(), nullptr, nullptr, 0, 0) == SOCKET_ERROR) {
 		if (WSAGetLastError() != WSAEWOULDBLOCK) {
 			CLog::WriteLog(L"Connect : Failed To Connect To Server! - %d", WSAGetLastError());
 			return false;
@@ -45,14 +45,14 @@ bool CTCPIPSocket::Connect(const FUNCTIONS::SOCKADDR::CSocketAddress& ConnectAdd
 
 bool CTCPIPSocket::Accept(const CTCPIPSocket& ListenSocket, NETWORK::UTIL::SESSION::SERVERSESSION::DETAIL::OVERLAPPED_EX& AcceptOverlapped) {
 	int Flag = 0, Size = sizeof(int);
-	if (getsockopt(GetSocketHandle(), SOL_SOCKET, SO_ACCEPTCONN, reinterpret_cast<char*>(&Flag), &Size) == SOCKET_ERROR) {
+	if (getsockopt(GetSocket(), SOL_SOCKET, SO_ACCEPTCONN, reinterpret_cast<char*>(&Flag), &Size) == SOCKET_ERROR) {
 		CLog::WriteLog(L"");
 		return false;
 	}
 
 	size_t AddrLen = FUNCTIONS::SOCKADDR::CSocketAddress::GetSize() + 16;
 	// AcceptEx의 네번 째 매개변수에는 0이 들어가야함. 그렇지 않으면 Accept와 동시의 데이터를 받겠다는 뜻이 되기 때문에 어떠한 데이터가 들어오기 전 까지는 블럭됨.
-	if (!AcceptEx(ListenSocket.GetSocketHandle(), GetSocketHandle(), GetReceiveBufferPtr(), 0, AddrLen, AddrLen, nullptr, &AcceptOverlapped.m_Overlapped)) {
+	if (!AcceptEx(ListenSocket.GetSocket(), GetSocket(), GetReceiveBufferPtr(), 0, AddrLen, AddrLen, nullptr, &AcceptOverlapped.m_Overlapped)) {
 		if (WSAGetLastError() != WSA_IO_PENDING && WSAGetLastError() != WSAEWOULDBLOCK) {
 			CLog::WriteLog(L"Accept : Failed To Accept - %d", WSAGetLastError());
 			return false;
@@ -62,11 +62,11 @@ bool CTCPIPSocket::Accept(const CTCPIPSocket& ListenSocket, NETWORK::UTIL::SESSI
 }
 
 bool CTCPIPSocket::Write(const char* const SendData, const size_t& DataLength, NETWORK::UTIL::SESSION::SERVERSESSION::DETAIL::OVERLAPPED_EX& SendOverlapped) {
-	return UTIL::TCPIP::Send(GetSocketHandle(), SendData, DataLength, SendOverlapped);
+	return UTIL::TCPIP::Send(GetSocket(), SendData, DataLength, SendOverlapped);
 }
 
 bool CTCPIPSocket::Read(char* const ReadBuffer, size_t&& ReadedSize, NETWORK::UTIL::SESSION::SERVERSESSION::DETAIL::OVERLAPPED_EX& RecvOverlapped) {
-	if (UTIL::TCPIP::Receive(GetSocketHandle(), GetReceiveBufferPtr(), ReadedSize, RecvOverlapped)) {
+	if (UTIL::TCPIP::Receive(GetSocket(), GetReceiveBufferPtr(), ReadedSize, RecvOverlapped)) {
 		if (ReadBuffer) {
 			CopyReceiveBuffer(ReadBuffer, ReadedSize);
 		}
@@ -76,8 +76,8 @@ bool CTCPIPSocket::Read(char* const ReadBuffer, size_t&& ReadedSize, NETWORK::UT
 }
 
 bool CTCPIPSocket::SocketRecycling(NETWORK::UTIL::SESSION::SERVERSESSION::DETAIL::OVERLAPPED_EX& DisconnectOverlapped) {
-	shutdown(GetSocketHandle(), SD_BOTH);
-	if (!TransmitFile(GetSocketHandle(), NULL, 0, 0, &DisconnectOverlapped.m_Overlapped, nullptr, TF_DISCONNECT | TF_REUSE_SOCKET)) {
+	shutdown(GetSocket(), SD_BOTH);
+	if (!TransmitFile(GetSocket(), NULL, 0, 0, &DisconnectOverlapped.m_Overlapped, nullptr, TF_DISCONNECT | TF_REUSE_SOCKET)) {
 		if (WSAGetLastError() != WSA_IO_PENDING) {
 			CLog::WriteLog(L"Socket Recycling Work Failure! - %d", WSAGetLastError());
 			return false;
