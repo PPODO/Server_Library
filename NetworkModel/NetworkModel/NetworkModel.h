90f72fc426d50f1c02df8a686d77b12e38553c03
PPODO <sherlockwhooo@gmail.com>
2019-11-01 15:28:33 +0900

    PacketQueueData를 BasePacket.hpp로 이동

--- NetworkModel/NetworkModel/NetworkModel.h
+++ NetworkModel/NetworkModel/NetworkModel.h
@@ -8,65 +8,8 @@
 #include <Functions/Functions/CircularQueue/CircularQueue.hpp>
 #include <Functions/Functions/Command/Command.h>
 
-namespace FUNCTIONS {
-	namespace CIRCULARQUEUE {
-		namespace QUEUEDATA {
-			struct CPacketQueueData : public QUEUEDATA::DETAIL::BaseData<CPacketQueueData, 500> {
-			public:
-				/*
-				EventSelect - CEventSelect
-				IOCP - CONNECTION
-				*/
-				void* m_Owner;
-				NETWORK::PACKET::PACKET_STRUCTURE m_PacketStructure;
-
-			public:
-				CPacketQueueData() : m_Owner(nullptr) { ZeroMemory(&m_PacketStructure, sizeof(NETWORK::PACKET::PACKET_STRUCTURE)); };
-				CPacketQueueData(void* const Owner, const NETWORK::PACKET::PACKET_STRUCTURE& PacketStructure) : m_Owner(Owner), m_PacketStructure(PacketStructure) {};
-
-			};
-		}
-	}
-}
-
 namespace NETWORKMODEL {
 	namespace DETAIL {
-		struct CONNECTION {
-			struct TCPCONNECTION {
-			public:
-				NETWORK::SESSION::SERVERSESSION::CServerSession* const m_Session;
-				FUNCTIONS::SOCKADDR::CSocketAddress m_Address;
-
-			public:
-				TCPCONNECTION() : m_Session(nullptr), m_Address() {};
-				TCPCONNECTION(NETWORK::SESSION::SERVERSESSION::CServerSession* const Session) : m_Session(Session), m_Address() {};
-				TCPCONNECTION(NETWORK::SESSION::SERVERSESSION::CServerSession* const Session, const FUNCTIONS::SOCKADDR::CSocketAddress& Address) : m_Session(Session), m_Address(Address) {};
-				TCPCONNECTION(const TCPCONNECTION& lvalue) : m_Session(lvalue.m_Session), m_Address(lvalue.m_Address) {};
-				TCPCONNECTION(const TCPCONNECTION&& rvalue) : m_Session(rvalue.m_Session), m_Address(rvalue.m_Address) {};
-
-			public:
-				bool operator==(const NETWORK::SESSION::SERVERSESSION::CServerSession* Session) const {
-					if (m_Session == Session) { return true; } return false;
-				}
-				bool operator==(const FUNCTIONS::SOCKADDR::CSocketAddress& const Address) const {
-					if (m_Address == Address) { return true; } return false;
-				}
-
-			};
-		public:
-			TCPCONNECTION m_Client;
-			NETWORK::SOCKET::UDPIP::PEERINFO m_Peer;
-
-		public:
-			CONNECTION() : m_Client(), m_Peer() {};
-			CONNECTION(const TCPCONNECTION& Client) : m_Client(Client), m_Peer() {};
-			CONNECTION(const NETWORK::SOCKET::UDPIP::PEERINFO& Peer) : m_Client(nullptr), m_Peer(Peer) {};
-			CONNECTION(const TCPCONNECTION& Client, const NETWORK::SOCKET::UDPIP::PEERINFO& Peer) : m_Client(Client), m_Peer(Peer) {};
-			CONNECTION(CONNECTION& lvalue) : m_Client(lvalue.m_Client), m_Peer(lvalue.m_Peer) {};
-			CONNECTION(CONNECTION&& rvalue) noexcept : m_Client(rvalue.m_Client), m_Peer(rvalue.m_Peer) {};
-
-		};
-
 		typedef std::map<uint8_t, std::function<void(FUNCTIONS::CIRCULARQUEUE::QUEUEDATA::CPacketQueueData* const)>> PACKETPROCESSORLIST;
 
 		class CNetworkModel {
