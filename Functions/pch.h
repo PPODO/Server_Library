9010ae01b53440aa23fa0f1a714a1338fd989b34
PPODO <sherlockwhooo@gmail.com>
2023-03-16 04:04:02 +0900

    Update Functions

---
+++ Functions/pch.h
@@ -0,0 +1,11 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "framework.h"
+#include "CriticalSection/CriticalSection.hpp"
+#include "MemoryLeak/MemoryLeak.hpp"
+#include "Log/Log.hpp"
+#include "Minidump/Minidump.hpp"
+#include "Uncopyable/Uncopyable.hpp"
+
+#endif //PCH_H
