49c72b7dec175aa0c9b6a9dbc2608d88f8ee5e66
PPODO <sherlockwhooo@gmail.com>
2023-03-21 02:12:12 +0900

    Create NetworkModel Repo

---
+++ NetworkModel/NetworkModel/BaseModel/BaseModel.cpp
@@ -0,0 +1,80 @@
+#include "BaseModel.hpp"
+#include <cassert>
+#include <Functions/Log/Log.hpp>
+
+using namespace SERVER::NETWORKMODEL::BASEMODEL;
+using namespace SERVER::FUNCTIONS::LOG;
+
+BaseNetworkModel::BaseNetworkModel(const int iPacketProcessorLoopCount, const PACKETPROCESSOR& packetProcessorMap) : m_iPacketProcessorLoopCount(iPacketProcessorLoopCount), m_packetProcessorMap(packetProcessorMap){
+	if (WSAStartup(WINSOCK_VERSION, &m_wsaData) != 0)
+		assert(false);
+
+	try {
+		m_pPacketProcessQueue = std::make_unique<PACKETQUEUE>();
+		m_pPacketStorageQueue = std::make_unique<PACKETQUEUE>();
+	}
+	catch (std::bad_alloc& exception) {
+		Log::WriteLog(L"Bad Alloc! - %s", exception.what());
+		assert(false);
+	}
+}
+
+BaseNetworkModel::~BaseNetworkModel() {
+	WSACleanup();
+}
+
+void BaseNetworkModel::Run() {
+	for (int i = 0; i < m_iPacketProcessorLoopCount; i++) {
+		NETWORK::PACKET::PacketQueueData* pPacketData = nullptr;
+		if (m_pPacketProcessQueue->Pop(pPacketData)) {
+			auto processor = m_packetProcessorMap.find(pPacketData->m_packetData.m_packetInfo.m_iPacketType);
+			if (processor != m_packetProcessorMap.cend())
+				processor->second(pPacketData);
+
+			delete pPacketData;
+		}
+		else if (m_pPacketProcessQueue->IsEmpty() && !m_pPacketStorageQueue->IsEmpty()) {
+			FUNCTIONS::CRITICALSECTION::CriticalSectionGuard lock(m_packetQueueLock);
+
+			m_pPacketProcessQueue.swap(m_pPacketStorageQueue);
+			m_pPacketProcessQueue->EnableCriticalSection(false);
+			m_pPacketStorageQueue->EnableCriticalSection(true);
+
+			// 패킷 프로세스 큐는 메인 스레드에서만 접근함. 크리티컬 섹션을 사용할 필요가 없음.
+			// 크리티컬 섹션은 상당한 자원을 소모한다. 필요없으면 안 쓰는 게 좋음 ㅇㅡㅇ
+		}
+	}
+}
+
+void BaseNetworkModel::ReceiveDataProcessing(const EPROTOCOLTYPE protocolType, char* const sReceiveBuffer, uint16_t iReceiveBytes, int16_t& iLastReceivePacketNumber, void* const pOwner) {
+	using namespace NETWORK::PACKET;
+	
+	const size_t PACKET_INFO_STRUCT_SIZE = PACKET_INFORMATION::GetStructSize();
+
+	while (true) {
+		PACKET_STRUCT packetStruct;
+		uint16_t iRemainBytes = iReceiveBytes;
+
+		// 패킷 헤더만큼 수신 했는지 확인 후, 수신 했다면 헤더 가져옴
+		if (iReceiveBytes > PACKET_INFO_STRUCT_SIZE) {
+			packetStruct.m_packetInfo = *reinterpret_cast<PACKET_INFORMATION*>(sReceiveBuffer);
+			iRemainBytes -= PACKET_INFO_STRUCT_SIZE;
+		}
+
+		if (iReceiveBytes >= packetStruct.m_packetInfo.m_iPacketSize) {
+			uint16_t iPacketTotalBytes = PACKET_INFO_STRUCT_SIZE + packetStruct.m_packetInfo.m_iPacketSize;
+			if (packetStruct.m_packetInfo.m_iPacketNumber == iLastReceivePacketNumber) { // for udp, tcp has always 0
+				if (m_protocolType & EPROTOCOLTYPE::EPT_UDP) InterlockedIncrement16(&iLastReceivePacketNumber);
+				iRemainBytes -= packetStruct.m_packetInfo.m_iPacketSize;
+				CopyMemory(packetStruct.m_sPacketData, sReceiveBuffer + PACKET_INFO_STRUCT_SIZE, packetStruct.m_packetInfo.m_iPacketSize);
+
+				FUNCTIONS::CRITICALSECTION::CriticalSectionGuard lock(m_packetQueueLock);
+				m_pPacketStorageQueue->Push(new PacketQueueData(pOwner, packetStruct));
+			}
+			MoveMemory(sReceiveBuffer, sReceiveBuffer + iPacketTotalBytes, iRemainBytes);
+			iReceiveBytes -= iPacketTotalBytes;
+		}
+		else
+			break;
+	}
+}
---
+++ NetworkModel/NetworkModel/BaseModel/BaseModel.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <Network/UserSession/Server/User_Server.hpp>
+#include <Network/Packet/BasePacket.hpp>
+#include <map>
+#include <functional>
+
+using namespace SERVER::NETWORK::PROTOCOL::UTIL::BSD_SOCKET;
+
+namespace SERVER {
+	namespace NETWORKMODEL {
+		namespace BASEMODEL {
+			typedef std::map<uint8_t, std::function<void(NETWORK::PACKET::PacketQueueData* const)>> PACKETPROCESSOR;
+
+			class BaseNetworkModel {
+				typedef FUNCTIONS::CIRCULARQUEUE::CircularQueue<NETWORK::PACKET::PacketQueueData*> PACKETQUEUE;
+			private:
+				const PACKETPROCESSOR& m_packetProcessorMap;
+				const int m_iPacketProcessorLoopCount;
+
+				WSADATA m_wsaData;
+
+				EPROTOCOLTYPE m_protocolType;
+
+				FUNCTIONS::CRITICALSECTION::CriticalSection m_packetQueueLock;
+				std::unique_ptr<PACKETQUEUE> m_pPacketProcessQueue;
+				std::unique_ptr<PACKETQUEUE> m_pPacketStorageQueue;
+
+			protected:
+				virtual void Destroy() = 0;
+
+			public:
+				BaseNetworkModel(const int iPacketProcessorLoopCount, const PACKETPROCESSOR& packetProcessorMap);
+				virtual ~BaseNetworkModel();
+
+			public:
+				virtual bool Initialize(const EPROTOCOLTYPE protocolType, const FUNCTIONS::SOCKETADDRESS::SocketAddress& serverAddress) = 0 {
+					m_protocolType = protocolType;
+
+					return true;
+				}
+
+				virtual void Run();
+
+				void ReceiveDataProcessing(const EPROTOCOLTYPE protocolType, char* const sReceiveBuffer, uint16_t iReceiveBytes, int16_t& iLastReceivePacketNumber, void* const pOwner);
+
+			public:
+				inline EPROTOCOLTYPE GetProtocolType() const { return m_protocolType; }
+
+			};
+		}
+	}
+}
