791c1a3778f95edb6660b41e058742685ca6b454
PPODO <sherlockwhooo@gmail.com>
2023-03-24 02:21:36 +0900

    Update Test Project

--- Server_test/main.cpp
+++ Server_test/main.cpp
@@ -1,46 +1,82 @@
 #include <iostream>
 #include <Network/UserSession/Server/User_Server.hpp>
+#include <Network/NetworkProtocol/UDPSocket.hpp>
+#include <Network/NetworkProtocol/TCPSocket.hpp>
 #include <thread>
 
 int main() {
-	using namespace SERVER::NETWORK::USER_SESSION::USER_SERVER;
-
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	User_Server server(SERVER::NETWORK::PROTOCOL::UTIL::BSD_SOCKET::EPROTOCOLTYPE::EPT_TCP);
+	// udp test
+	{
+		using namespace SERVER::NETWORK::PROTOCOL::UDP;
 
-	SERVER::FUNCTIONS::SOCKETADDRESS::SocketAddress address(1350);
-	server.Initialize(address);
+		UDPIPSocket udpSocket;
+		SERVER::FUNCTIONS::SOCKETADDRESS::SocketAddress bindAddress(3550);
 
-	std::thread t1([&]() {
-		SERVER::NETWORK::PROTOCOL::TCP::TCPIPSocket client;
-	
-	while (!client.Connect(address));
-
-	std::cout << "Connect!\n";
-
-	char messageBuffer[1024] = { "\0" };
-	uint16_t readbytes;
-	while (true) {
-		if (client.Read(messageBuffer, readbytes)) {
-			std::cout << messageBuffer << std::endl;
-			break;
+		if (!udpSocket.Bind(bindAddress)) {
+			std::cout << "UDP Bind Failed!\n";
+			return -1;
 		}
+
+		std::thread t1([]() {
+			UDPIPSocket clientSocket;
+		SERVER::FUNCTIONS::SOCKETADDRESS::SocketAddress serverAddress(3550);
+		char messageBuffer[] = { "Hello World!\n" };
+
+		for (int i = 0; i < 100; i++)
+			clientSocket.WriteTo(serverAddress, messageBuffer, strlen(messageBuffer));
+			});
+
+		char messageBuffer[1024] = { "\0" };
+		uint16_t receivedBytes = 0;
+
+		for (int i = 0; i < 100; i++) {
+			udpSocket.ReadFrom(messageBuffer, receivedBytes);
+			if (receivedBytes > 0)
+				break;
+		}
+
+		std::cout << messageBuffer << std::endl;
+
+		t1.join();
 	}
 
-	});
+	// tcp test
+	{
+		using namespace SERVER::NETWORK::PROTOCOL::TCP;
 
-	User_Server client(SERVER::NETWORK::PROTOCOL::UTIL::BSD_SOCKET::EPROTOCOLTYPE::EPT_TCP);
-	
-	while (!client.Initialize(server));
+		TCPIPSocket tcpSocket;
+		SERVER::FUNCTIONS::SOCKETADDRESS::SocketAddress bindAddress(1350);
 
-	char message[1024] = "QWEQWE";
-	while (true) {
-		if (client.Send(message, 4)) break;
+		tcpSocket.Bind(bindAddress);
+		tcpSocket.Listen();
+
+		std::thread t1([&]() {
+			TCPIPSocket clientSocket;
+		while (!clientSocket.Connect(bindAddress));
+
+		char messageBuffer[] = { "Hello World!!!\n" };
+		for (int i = 0; i < 10; i++) {
+			clientSocket.Write(messageBuffer, strlen(messageBuffer));
+		}
+			});
+
+		SERVER::NETWORK::USER_SESSION::USER_SERVER::OVERLAPPED_EX overlap;
+		TCPIPSocket clientSocket;
+		while (!clientSocket.Accept(tcpSocket, overlap));
+
+		char messageBuffer[1024] = { "\0" };
+		uint16_t recvBytes = 0;
+		for (int i = 0; i < 10; i++) {
+			clientSocket.Read(messageBuffer, recvBytes);
+		}
+
+		std::cout << messageBuffer << std::endl;
+
+		t1.join();
 	}
-
-	t1.join();
 	WSACleanup();
 	return 0;
 }
