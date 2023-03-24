f8f60431cdd8ac337dade8a12eb26936a62b0232
PPODO <sherlockwhooo@gmail.com>
2023-03-25 02:34:42 +0900

    Update NetworkModel

--- NetworkModel/NetworkModel/IOCP/IOCP.cpp
+++ NetworkModel/NetworkModel/IOCP/IOCP.cpp
@@ -1,4 +1,5 @@
 #include "IOCP.hpp"
+#include <MSWSock.h>
 
 using namespace SERVER::NETWORKMODEL::IOCP;
 using namespace SERVER::NETWORKMODEL::BASEMODEL;
@@ -71,8 +72,153 @@ void IOCP::Destroy() {
 }
 
 void IOCP::IOCPWorkerThread() {
+    using namespace NETWORK::USER_SESSION::USER_SERVER;
 
     while (true) {
+        DWORD iRecvBytes = 0;
+        void* pCompletionKey = nullptr;
+        OVERLAPPED_EX* pOverlappedEx = nullptr;
 
+        bool bIOCPResult = GetQueuedCompletionStatus(m_hIOCP, &iRecvBytes, reinterpret_cast<PULONG_PTR>(&pCompletionKey), reinterpret_cast<LPOVERLAPPED*>(&pOverlappedEx), INFINITE);
+
+        if (!pCompletionKey) // stop event
+            break;
+
+        if (pOverlappedEx) {
+            if (!bIOCPResult || iRecvBytes <= 0) {
+                switch (pOverlappedEx->m_IOType) {
+                case EIOTYPE::EIT_ACCEPT:
+                    OnIOAccept(pOverlappedEx);
+                    break;
+                case EIOTYPE::EIT_DISCONNECT:
+                    OnIODisconnect(pOverlappedEx->m_pOwner);
+                    break;
+                default:
+                    OnIOTryDisconnect(pOverlappedEx->m_pOwner);
+                    break;
+                }
+            }
+            continue;
+        }
+
+        switch (pOverlappedEx->m_IOType) {
+        case EIOTYPE::EIT_READ:
+            OnIOReceive(pOverlappedEx, iRecvBytes);
+            break;
+        case EIOTYPE::EIT_WRITE:
+            OnIOWrite(pOverlappedEx->m_pOwner, iRecvBytes);
+            break;
+        case EIOTYPE::EIT_READFROM:
+            OnIOReceiveFrom(pOverlappedEx, iRecvBytes);
+            break;
+        }
     }
+}
+
+CONNECTION* IOCP::GetConnectionFromList(User_Server* const pUser) {
+    auto iterator = std::find_if(m_clientList.cbegin(), m_clientList.cend(), [&pUser](CONNECTION* const pConnection) {
+        if (pConnection->m_pUser == pUser) return true;
+        return false;
+    });
+
+    if (iterator != m_clientList.cend())
+        return *iterator;
+    return nullptr;
+}
+
+CONNECTION* IOCP::GetConnectionFromList(FUNCTIONS::SOCKETADDRESS::SocketAddress& peerAddress) {
+    auto iterator = std::find_if(m_clientList.cbegin(), m_clientList.cend(), [&peerAddress](CONNECTION* const pConnection) {
+        if (peerAddress == pConnection->m_peerInformation.m_remoteAddress) return true;
+        return false;
+    });
+
+    if (iterator != m_clientList.cend())
+        return *iterator;
+    else if (GetProtocolType() & EPROTOCOLTYPE::EPT_UDP) {
+        auto pNewConnection = new CONNECTION(m_pServer.get(), PeerInfo(peerAddress, 0));
+        m_clientList.emplace_back(pNewConnection);
+        return pNewConnection;
+    }
+    return nullptr;
+}
+
+
+CONNECTION* IOCP::OnIOAccept(OVERLAPPED_EX* const pAcceptOverlapped) {
+    using namespace FUNCTIONS::SOCKETADDRESS;
+
+    if (auto pUser = pAcceptOverlapped->m_pOwner) {
+        const DWORD iAddrSize = SocketAddress::GetSize() + 16;
+        sockaddr* pLocalAddr = nullptr, * pRemoteAddr = nullptr;
+        int iLocalLength = 0, iRemoteLength = 0;
+        GetAcceptExSockaddrs(pAcceptOverlapped->m_pReceiveBuffer, 0, iAddrSize, iAddrSize, &pLocalAddr, &iLocalLength, &pRemoteAddr, &iRemoteLength);
+
+        if (auto pRemoteAddr_in = reinterpret_cast<sockaddr_in*>(pRemoteAddr)) {
+            if (auto pConnection = GetConnectionFromList(pUser)) {
+                pConnection->m_peerInformation = PeerInfo(SocketAddress(*pRemoteAddr_in), 0);
+                if (pUser->RegisterIOCompletionPort(m_hIOCP) && pUser->Receive()) {
+                    Log::WriteLog(L"Accept New Client!");
+                    return nullptr;
+                }
+            }
+        }
+        Log::WriteLog(L"Accept Failed!");
+        pUser->SocketRecycle();
+    }
+    return nullptr;
+}
+
+CONNECTION* IOCP::OnIODisconnect(User_Server* const pClient) {
+    if (auto pConnection = GetConnectionFromList(pClient)) {
+        if (pClient->Initialize(*m_pServer.get())) {
+            Log::WriteLog(L"Disconnect Client!");
+            return pConnection;
+        }
+    }
+    return nullptr;
+}
+
+CONNECTION* IOCP::OnIOWrite(User_Server* const pClient, const uint16_t iWriteBytes) {
+    pClient->SendCompletion(EPROTOCOLTYPE::EPT_TCP, iWriteBytes);
+    return GetConnectionFromList(pClient);
+}
+
+CONNECTION* IOCP::OnIOReceive(OVERLAPPED_EX* const pReceiveOverlapped, const uint16_t iRecvBytes) {
+    if (auto pConnection = GetConnectionFromList(pReceiveOverlapped->m_pOwner)) {
+
+        pReceiveOverlapped->m_iRemainReceiveBytes += iRecvBytes;
+        ReceiveDataProcessing(EPROTOCOLTYPE::EPT_TCP, pReceiveOverlapped->m_pReceiveBuffer, pReceiveOverlapped->m_iRemainReceiveBytes, pReceiveOverlapped->m_iLastReceivedPacketNumber, pConnection->m_pUser);
+        if (!pConnection->m_pUser->Receive()) {
+            pConnection->m_pUser->SocketRecycle();
+        }
+        return pConnection;
+    }
+    return nullptr;
+}
+
+CONNECTION* IOCP::OnIOReceiveFrom(OVERLAPPED_EX* const pReceiveFromOverlapped, const uint16_t iRecvBytes) {
+    if (auto pConnection = GetConnectionFromList(pReceiveFromOverlapped->m_pOwner)) {
+        pReceiveFromOverlapped->m_iRemainReceiveBytes += iRecvBytes;
+        pReceiveFromOverlapped->m_iLastReceivedPacketNumber = pConnection->m_peerInformation.m_iLastPacketNumber;
+
+        if (NETWORK::PROTOCOL::UTIL::UDP::CheckAck(*pReceiveFromOverlapped)) {
+            ReceiveDataProcessing(EPROTOCOLTYPE::EPT_UDP, pReceiveFromOverlapped->m_pReceiveBuffer, pReceiveFromOverlapped->m_iRemainReceiveBytes, pReceiveFromOverlapped->m_iLastReceivedPacketNumber, pConnection->m_pUser);
+            pConnection->m_peerInformation.m_iLastPacketNumber = pReceiveFromOverlapped->m_iLastReceivedPacketNumber;
+        }
+
+        if (!pConnection->m_pUser->ReceiveFrom())
+            pConnection->m_pUser->SocketRecycle();
+        return pConnection;
+    }
+    return nullptr;
+}
+
+CONNECTION* IOCP::OnIOTryDisconnect(User_Server* const pClient) {
+    if (auto pConnection = GetConnectionFromList(pClient)) {
+        if (pConnection->m_pUser->SocketRecycle())
+            Log::WriteLog(L"Try Disconnect Client!");
+        else
+            OnIODisconnect(pClient);
+        return pConnection;
+    }
+    return nullptr;
 }
--- NetworkModel/NetworkModel/IOCP/IOCP.hpp
+++ NetworkModel/NetworkModel/IOCP/IOCP.hpp
@@ -3,23 +3,27 @@
 #include <vector>
 #include <thread>
 
+using namespace SERVER::NETWORK::USER_SESSION::USER_SERVER;
+
 namespace SERVER {
 	namespace NETWORKMODEL {
 		namespace IOCP {
 			struct CONNECTION {
 			public:
-				NETWORK::USER_SESSION::USER_SERVER::User_Server* m_pUser;
+				User_Server* m_pUser;
+				NETWORK::PROTOCOL::UDP::PeerInfo m_peerInformation;
 
 			public:
-				CONNECTION() : m_pUser(nullptr) {};
-				CONNECTION(NETWORK::USER_SESSION::USER_SERVER::User_Server* pUser) : m_pUser(pUser) {};
+				CONNECTION() : m_pUser(nullptr), m_peerInformation() {};
+				CONNECTION(User_Server* pUser) : m_pUser(pUser), m_peerInformation() {};
+				CONNECTION(User_Server* pUser, const NETWORK::PROTOCOL::UDP::PeerInfo& peerInfo) : m_pUser(pUser), m_peerInformation(peerInfo) {};
 
 			};
 
 			class IOCP : private BASEMODEL::BaseNetworkModel {
 			private:
 				HANDLE m_hIOCP;
-				std::unique_ptr<NETWORK::USER_SESSION::USER_SERVER::User_Server> m_pServer;
+				std::unique_ptr<User_Server> m_pServer;
 
 				std::vector<std::thread> m_workerThreadList;
 
@@ -35,8 +39,18 @@ namespace SERVER {
 				virtual void Run() override;
 				virtual void Destroy() override;
 
+				virtual CONNECTION* OnIOAccept(OVERLAPPED_EX* const pAcceptOverlapped);
+				virtual CONNECTION* OnIODisconnect(User_Server* const pClient);
+				virtual CONNECTION* OnIOWrite(User_Server* const pClient, const uint16_t iWriteBytes);
+				virtual CONNECTION* OnIOReceive(OVERLAPPED_EX* const pReceiveOverlapped, const uint16_t iRecvBytes);
+				virtual CONNECTION* OnIOReceiveFrom(OVERLAPPED_EX* const pReceiveFromOverlapped, const uint16_t iRecvBytes);
+				virtual CONNECTION* OnIOTryDisconnect(User_Server* const pClient);
+
 			private:
 				void IOCPWorkerThread();
+
+				CONNECTION* GetConnectionFromList(User_Server* const pUser);
+				CONNECTION* GetConnectionFromList(FUNCTIONS::SOCKETADDRESS::SocketAddress& peerAddress);
 
 			};
 
