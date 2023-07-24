92c16f737c6dc6d4d0430526a7b1271de31a8700
PPODO <sherlockwhooo@gmail.com>
2023-07-25 02:13:10 +0900

    test code

--- Server_test/main.cpp
+++ Server_test/main.cpp
@@ -1,121 +1,33 @@
 #include <iostream>
-#include <NetworkModel/BaseModel/BaseModel.hpp>
-#include <NetworkModel/IOCP/IOCP.hpp>
-#include <thread>
+#include <Functions/MySQL/MySQL.hpp>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/datatype.h>
+#include <MySQLDataFormatGenerator/dbtest_jon.h>
 
-void pk(SERVER::NETWORK::PACKET::PacketQueueData* const pPacketData) {
-
-}
+using namespace SERVER::FUNCTIONS::MYSQL;
 
 int main() {
-/*	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
-
-	// udp test
+	CMySQLPool mysql("tcp://localhost:3306", "root", "a2233212.", 16);
 	{
-		using namespace SERVER::NETWORK::PROTOCOL::UDP;
+		CMySQLPool::CSQLRealConnection pConnection = mysql.GetConnection("testdb");
 
-		UDPIPSocket udpSocket;
-		SERVER::FUNCTIONS::SOCKETADDRESS::SocketAddress bindAddress(3550);
+		try {
+			CDBTEST_JON test_jon(501, "Winter");
+			/*test_jon.ExecuteQueryForInsert(pConnection, "dbtest_jon");*/
 
-		if (!udpSocket.Bind(bindAddress)) {
-			std::cout << "UDP Bind Failed!\n";
-			return -1;
-		}
+			std::vector<CDBTEST_JON> listOfOutput;
+			test_jon.ExecuteQueryForSelect(pConnection, "dbtest_jon", listOfOutput, {}, { SQL::CColumnLabelValuePair("user_name", "Winter"), SQL::CColumnLabelValuePair("id", "5") }, { "OR" });
 
-		std::thread t1([]() {
-			UDPIPSocket clientSocket;
-		SERVER::FUNCTIONS::SOCKETADDRESS::SocketAddress serverAddress(3550);
-		char messageBuffer[] = { "Hello World!\n" };
-
-		for (int i = 0; i < 100; i++)
-			clientSocket.WriteTo(serverAddress, messageBuffer, strlen(messageBuffer));
-			});
-
-		char messageBuffer[1024] = { "\0" };
-		uint16_t receivedBytes = 0;
-
-		for (int i = 0; i < 100; i++) {
-			udpSocket.ReadFrom(messageBuffer, receivedBytes);
-			if (receivedBytes > 0)
-				break;
-		}
-
-		std::cout << messageBuffer << std::endl;
-
-		t1.join();
-	}
-
-	// tcp test
-	{
-		using namespace SERVER::NETWORK::PROTOCOL::TCP;
-
-		TCPIPSocket tcpSocket;
-		SERVER::FUNCTIONS::SOCKETADDRESS::SocketAddress bindAddress(1350);
-
-		tcpSocket.Bind(bindAddress);
-		tcpSocket.Listen();
-
-		std::thread t1([&]() {
-			TCPIPSocket clientSocket;
-		while (!clientSocket.Connect(bindAddress));
-
-		char messageBuffer[] = { "Hello World!!!\n" };
-		for (int i = 0; i < 10; i++) {
-			clientSocket.Write(messageBuffer, strlen(messageBuffer));
-		}
-			});
-
-		SERVER::NETWORK::USER_SESSION::USER_SERVER::OVERLAPPED_EX overlap;
-		TCPIPSocket clientSocket;
-		while (!clientSocket.Accept(tcpSocket, overlap));
-
-		char messageBuffer[1024] = { "\0" };
-		uint16_t recvBytes = 0;
-		for (int i = 0; i < 10; i++) {
-			clientSocket.Read(messageBuffer, recvBytes);
-		}
-
-		std::cout << messageBuffer << std::endl;
-
-		t1.join();
-	}
-	WSACleanup();*/
-
-	{
-		using namespace SERVER::NETWORKMODEL::IOCP;
-
-		SERVER::NETWORKMODEL::BASEMODEL::PACKETPROCESSOR processor;
-		processor.insert(std::make_pair<uint8_t, void(SERVER::NETWORK::PACKET::PacketQueueData*)>(1, pk));
-
-		SERVER::FUNCTIONS::SOCKETADDRESS::SocketAddress address(1335);
-
-		IOCP iocp(processor, 1);
-
-		iocp.Initialize(SERVER::NETWORK::PROTOCOL::UTIL::BSD_SOCKET::EPROTOCOLTYPE::EPT_TCP, address);
-
-
-		std::thread t1([&address]() {
-			SERVER::NETWORK::PROTOCOL::TCP::TCPIPSocket tcpip;
-
-			tcpip.Connect(address);
-
-			char messageBuffer[1024];
-			while (true) {
-				std::cin >> messageBuffer;
-
-				if (!tcpip.Write(messageBuffer, strlen(messageBuffer))) {
-					std::cout << "send failed\n";
-				}
+			for (auto It : listOfOutput) {
+				std::cout << It.m_ID.m_sColumnLabel << '\t' << It.m_ID.m_rawData << '\t';
+				std::cout << It.m_user_name.m_sColumnLabel << '\t' << It.m_user_name.m_rawData << std::endl;
 			}
-		});
-
-		while (true) {
-			iocp.Run();
 		}
+		catch (sql::SQLException& exception) {
+			std::cout << exception.what() << std::endl;
+		}
+		//state->setInt(1, 100);
 
-		t1.join();
-		iocp.Destroy();
 	}
 
 	return 0;
